8186df0098994c587f8e8a45c3c5958f1936c89d
innovin <innovinanuonye@gmail.com>
2023-06-07 01:51:27 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useReducer, useEffect} from 'react'
+import './App.css'
+import './assets/style.css'
+
+const oldCount = localStorage.getItem('count')
+const initialCount = oldCount || {count: 0}
+
+const reducer = (state, action) => {
+    let returnValue;
+    action.type === 'INCREASE' && (returnValue = {count: state.count++})
+    action.type === 'DECREASE' && (returnValue = {count: state.count--})
+    action.type === 'RESET' && (returnValue = {count: 0})
+    return returnValue || {count: 0}
+}
+
+function App() {
+    const [counterState, dispatchCounter] = useReducer(reducer, initialCount);
+    const increase = () => {
+        dispatchCounter({type: 'INCREASE'})
+    }
+    const decrease = () => {
+        dispatchCounter({type: 'DECREASE'})
+    }
+    const reset = () => {
+        dispatchCounter({type: 'RESET'})
+    }
+
+    useEffect(() => {
+        counterState === 1 && (console.log('hmm'));
+
+    }, [counterState])
+
+    return (
+        <>
+
+            <h1>Boring Counter </h1>
+            <small>Nothing fancy, really!</small>
+            <div className="card">
+                <button onClick={decrease}>
+                    decrease :(
+                </button>
+                <button>
+                    count is {counterState.count}
+                </button>
+                <button onClick={increase}>
+                    increase :)
+                </button>
+
+
+            </div>
+            <button onClick={reset} >
+                Reset
+            </button>
+            <p>
+                Innovin...
+            </p>
+        </>
+    )
+}
+
+export default App
